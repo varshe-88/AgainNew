ccbbd100354b89bd5b2766f2ee40e7dfe4765ceb
vilas <vilas@LAPTOP-PURHK543>
2023-02-08 18:49:39 -0500

    commit

--- Finally/src/Lk.java
+++ Finally/src/Lk.java
@@ -4,7 +4,7 @@ public class Lk {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("hello");
-System.out.println("kghkjghk");
+System.out.println("kghkjghjfjfjhk");
 	}
 
 }
