9874c5530b3f51efe71a9e1e390032888066e71e
vilas <vilas@LAPTOP-PURHK543>
2023-02-08 20:36:11 -0500

    commit

--- Finally/src/GH.java
+++ Finally/src/GH.java
@@ -4,6 +4,7 @@ public class GH {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("welcome to second program");
+		System.out.println("vhvhvvh");
 
 	}
 
